03d7aa90943bcb58437ffa18afbd9d5f7282d5c1
Potter "GhG" Daniel <30369090+GhostHunterGal@users.noreply.github.com>
2023-04-16 06:12:24 -0400

    Updated ReadMe and New Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //App.js
 import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [balances, setBalances] = useState([]);
+  const [address, setAddress] = useState('');
 
   async function startMonitoring() {
-    const response = await fetch("http://localhost:3001/start-monitoring");
+    const response = await fetch(`http://localhost:3001/start-monitoring?address=${address}`);
   
     if (response.ok) {
       const newBalances = await response.json();
@@ -20,16 +22,31 @@ function App() {
       // or handle the error in another way that fits your application
     }
   }
-  
+
+  function handleAddressChange(event) {
+    setAddress(event.target.value);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Monitor Addresses</h1>
-        <button onClick={startMonitoring}>Start Monitoring</button>
-        <ul>
+        <h1 className="mb-4">Monitor Addresses</h1>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            value={address}
+            onChange={handleAddressChange}
+            placeholder="Enter wallet address"
+            className="form-control"
+            aria-label="Enter wallet address"
+          />
+          <button onClick={startMonitoring} className="btn btn-outline-light">
+            Start Monitoring
+          </button>
+        </div>
+        <ul className="list-group">
           {balances.map((balance, index) => (
-            <li key={index}>
+            <li key={index} className="list-group-item list-group-item-dark">
               Balance of {balance.address}: {balance.balance} ether
             </li>
           ))}
