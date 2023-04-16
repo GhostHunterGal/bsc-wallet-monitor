af024ad8149b00d13495e73205849ce19db73f2a
Potter "GhG" Daniel <30369090+GhostHunterGal@users.noreply.github.com>
2023-04-16 05:27:42 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+//App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [balances, setBalances] = useState([]);
+
+  async function startMonitoring() {
+    const response = await fetch("http://localhost:3001/start-monitoring");
+  
+    if (response.ok) {
+      const newBalances = await response.json();
+      console.log("New Balances", newBalances);
+      setBalances(newBalances);
+    } else {
+      const errorResponse = await response.json();
+      console.error("Error fetching balances:", errorResponse.error);
+      setBalances([]);
+      // Alternatively, you can show an error message to the user
+      // or handle the error in another way that fits your application
+    }
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Monitor Addresses</h1>
+        <button onClick={startMonitoring}>Start Monitoring</button>
+        <ul>
+          {balances.map((balance, index) => (
+            <li key={index}>
+              Balance of {balance.address}: {balance.balance} ether
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
